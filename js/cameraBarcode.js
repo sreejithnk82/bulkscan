3caccae0508c7c7eb090f1e5fd3c7045571b52e1
sreejithnk82 <puliyottumukku@gmail.com>
2026-01-28 07:31:26 +0530

    Update cameraBarcode.js

--- js/cameraBarcode.js
+++ js/cameraBarcode.js
@@ -1,12 +1,12 @@
 const BarcodeCamera = {
-    scanner:null,
-    start:function(onSuccess){
-        if(this.scanner) this.scanner.clear().catch(()=>{});
-        this.scanner = new Html5Qrcode("barcode-scanner-container");
-        this.scanner.start({facingMode:"environment"}, {fps:10, qrbox:{width:250,height:150}}, onSuccess)
-            .catch(err=>console.warn("Barcode camera start failed",err));
-    },
-    stop:function(){
-        if(this.scanner) this.scanner.stop().then(()=>this.scanner.clear()).catch(()=>{});
-    }
+  scanner:null,
+  start:function(onSuccess){
+    if(this.scanner) this.scanner.clear().catch(()=>{});
+    this.scanner = new Html5Qrcode("barcode-scanner-container");
+    this.scanner.start({facingMode:"environment"}, {fps:10, qrbox:{width:250,height:150}}, onSuccess)
+      .catch(err=>console.warn("Barcode camera start failed",err));
+  },
+  stop:function(){
+    if(this.scanner) this.scanner.stop().then(()=>this.scanner.clear()).catch(()=>{});
+  }
 };
