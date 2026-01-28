5dfda4abbeebec5d48d09ff4beb4e75e80f95bea
sreejithnk82 <puliyottumukku@gmail.com>
2026-01-28 07:32:11 +0530

    Update ocr.js

--- js/ocr.js
+++ js/ocr.js
@@ -1,6 +1,6 @@
 const OCR = {
-    recognize: async function(canvas){
-        const result = await Tesseract.recognize(canvas,'eng',{logger:m=>{}});
-        return result.data.text;
-    }
+  recognize: async function(canvas){
+    const result = await Tesseract.recognize(canvas,'eng',{logger:m=>{}});
+    return result.data.text;
+  }
 };
