ad9a8d090a292ca392a84c32dee6c05228962149
sreejithnk82 <puliyottumukku@gmail.com>
2026-01-28 07:32:35 +0530

    Update parser.js

--- js/parser.js
+++ js/parser.js
@@ -1,11 +1,11 @@
 const Parser = {
-    parse: function(text){
-        const lines = text.split("\n").map(l=>l.trim()).filter(l=>l);
-        let pin = (text.match(/\b[1-9][0-9]{5}\b/)||[''])[0];
-        let phone = (text.match(/\+?\d{10,12}/)||[''])[0];
-        let name = lines[0]||'';
-        let address = lines.join(' ');
-        let city = '';
-        return {name,phone,address,city,pin};
-    }
+  parse:function(text){
+    const lines = text.split("\n").map(l=>l.trim()).filter(l=>l);
+    const pin = (text.match(/\b[1-9][0-9]{5}\b/)||[''])[0];
+    const phone = (text.match(/\+?\d{10,12}/)||[''])[0];
+    const name = lines[0]||'';
+    const address = lines.join(' ');
+    const city = '';
+    return {name,phone,address,city,pin};
+  }
 };
